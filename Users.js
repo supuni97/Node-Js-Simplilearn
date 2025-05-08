ff796068a368bbac6060dec88cafffd2c4cbadd5
supuni97 <sitharabandara1997@gmail.com>
2025-05-08 11:51:48 +0530

    Added get all users api

---
+++ Users.js
@@ -0,0 +1,19 @@
+const users = [
+  {
+    id: 1,
+    name: "John",
+    email: "john@gmail.com",
+  },
+  {
+    id: 2,
+    name: "David",
+    email: "david@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Kamal",
+    email: "Kamal@gmail.com",
+  },
+];
+
+module.exports = users;
---
+++ routes/api/users.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const uuid = require("uuid");
+let users = require("../../Users");
+
+//get all users
+router.get("/", (req, res) => {
+  res.json(users);
+});
+
+//get user by id
+router.get("/", (req, res) => {
+  res.json(users);
+});
+
+module.exports = router;
